d31a311c406c5bf15f17555552f55e6a91aabf2f
frankdev7 <franklyncr12@gmail.com>
2022-09-26 09:48:02 -0500

    first commit

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "hardhat/console.sol";
+
+contract Marketplace is ReentrancyGuard {
+    address payable public immutable feeAccount;
+    uint public immutable feePercent;
+    uint public itemCount;
+
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    mapping(uint => Item) public items;
+
+    event Offered(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
+    event Bought(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller,
+        address indexed buyer
+    );
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+        console.log("deployed!");
+    }
+
+    modifier checkPrice(uint _price) {
+        require(_price > 0, "Price cant be less than zero");
+        _;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint _tokenId,
+        uint _price
+    ) external checkPrice(_price) nonReentrant {
+        itemCount++;
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
+    }
+
+    function purchaseItem(uint _itemId) external payable nonReentrant {
+        uint totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId < itemCount);
+        require(msg.value >= totalPrice);
+        require(!item.sold);
+        item.seller.transfer(item.price);
+        feeAccount.transfer(totalPrice - item.price);
+        item.sold = true;
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            item.tokenId,
+            item.price,
+            item.seller,
+            msg.sender
+        );
+    }
+
+    function getTotalPrice(uint _itemId) internal view returns (uint) {
+        return ((items[_itemId].price * (100 + feePercent)) / 100);
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(10);
+
+  await marketplace.deployed();
+
+  console.log(`deployed to ${marketplace.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
